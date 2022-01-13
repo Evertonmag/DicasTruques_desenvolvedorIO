af4ac6990df1bf569743d4ef9b1614aa9458ae0a
Everton Vicente <everton.vicente@onfriday.com.br>
2022-01-13 17:16:01 -0300

    Add project files.

---
+++ src/Data/ApplicationContext.cs
@@ -0,0 +1,105 @@
+﻿using Efcore.Tips.Domain;
+using Efcore.Tips.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efcore.Tips.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+        public DbSet<UsuarioFuncao>? UsuarioFuncoes { get; set; }
+        public DbSet<DepartamentoRelatorio>? DepartamentoRelatorio { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Insira a linha de conexao")
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        #region SemPrimarykey
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<UsuarioFuncao>().HasNoKey();
+        //}
+        #endregion
+
+        #region UsandoViewsDoBancoDeDados
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<DepartamentoRelatorio>(e =>
+        //    {
+        //        e.HasNoKey();
+
+        //        e.ToView("vw_departamento_relatorio");
+
+        //        e.Property(p => p.Departamento).HasColumnName("Descricao");
+        //    });
+        //}
+
+        #endregion
+
+        #region ForcandoVARCHAR
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<DepartamentoRelatorio>(e =>
+        //    {
+        //        e.HasNoKey();
+
+        //        e.ToView("vw_departamento_relatorio");
+
+        //        e.Property(p => p.Departamento).HasColumnName("Descricao");
+        //    });
+        //    var properties = modelBuilder.Model.GetEntityTypes()
+        //        .SelectMany(p => p.GetProperties())
+        //        .Where(p => p.ClrType == typeof(string)
+        //                && p.GetColumnType() == null);
+
+        //    foreach (var property in properties)
+        //    {
+        //        property.SetIsUnicode(false);
+        //    }
+        //}
+
+        #endregion
+
+        #region SnakeCaseExtensions
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DepartamentoRelatorio>(e =>
+            {
+                e.HasNoKey();
+
+                e.ToView("vw_departamento_relatorio");
+
+                e.Property(p => p.Departamento).HasColumnName("Descricao");
+            });
+            var properties = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(p => p.GetProperties())
+                .Where(p => p.ClrType == typeof(string)
+                        && p.GetColumnType() == null);
+
+            foreach (var property in properties)
+            {
+                property.SetIsUnicode(false);
+            }
+
+
+            modelBuilder.ToSnakeCaseNames();
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,195 @@
+﻿using Efcore.Tips.Data;
+using Efcore.Tips.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Tips
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContadorDeEventos();
+        }
+        
+        static void ContadorDeEventos()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            Console.WriteLine($"PID: {System.Diagnostics.Process.GetCurrentProcess().Id}");
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                var departamento = new Departamento
+                {
+                    Descricao = $"Departamento sem colaborador"
+                };
+
+                db.Departamentos?.Add(departamento);
+                db.SaveChanges();
+
+                _ = db.Departamentos?.Find(1);
+                _ = db.Departamentos?.AsNoTracking().FirstOrDefault();
+            }
+        }
+
+        static void OperadoresDeAgregacaoNoAgrupamento()
+        {
+            using var db = new ApplicationContext();
+
+            var sql = db.Departamentos?
+                .GroupBy(p => p.Descricao)
+                .Where(p=>p.Count() > 1)
+                .Select(p => new
+                {
+                    Descricao = p.Key,
+                    Contador = p.Count(),
+                    Media = p.Average(p => p.Id),
+                    Maximo = p.Max(p => p.Id),
+                    Soma = p.Sum(p => p.Id),
+                }).ToQueryString();
+
+            Console.WriteLine(sql);
+        }
+
+        static void OperadoresDeAgregacao()
+        {
+            using var db = new ApplicationContext();
+
+            var sql = db.Departamentos?
+                .GroupBy(p => p.Descricao)
+                .Select(p => new
+                {
+                    Descricao = p.Key,
+                    Contador = p.Count(),
+                    Media = p.Average(p => p.Id),
+                    Maximo = p.Max(p => p.Id),
+                    Soma = p.Sum(p => p.Id),
+                }).ToQueryString();
+
+            Console.WriteLine(sql);
+        }
+
+        static void NaoUnicode()
+        {
+            using var db = new ApplicationContext();
+
+            var sql = db.Database.GenerateCreateScript();
+
+            Console.WriteLine(sql);
+        }
+
+        static void ToView()
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Database.ExecuteSqlRaw(
+                @"CREATE VIEW vw_departamento_relatorio AS
+                SELECT
+                    d.Descricao, count(c.Id) as Colaboradores
+                FROM Departamentos d
+                LEFT JOIN Colaboradores c ON c.DepartamentoId=d.Id
+                GROUP BY d.Descricao");
+
+            var deprtamentos = Enumerable.Range(1, 10)
+                .Select(p => new Departamento
+                {
+                    Descricao = $"Departamento {p}",
+                    Colaboradores = Enumerable.Range(1, p)
+                        .Select(c => new Colaborador
+                        {
+                            Nome = $"Colaborador: {p}-{c}"
+                        }).ToList()
+                });
+
+            var departamento = new Departamento
+            {
+                Descricao = $"Departamento sem Colaborador"
+            };
+
+            db.Departamentos.Add(departamento);
+            db.Departamentos.AddRange(deprtamentos);
+            db.SaveChanges();
+
+            var relatorio = db.DepartamentoRelatorio
+                .Where(p=>p.Colaboradores < 20)
+                .OrderBy(p=>p.Departamento)
+                .ToList();
+
+            foreach (var dep in relatorio)
+            {
+                Console.WriteLine($"{dep.Departamento} [ Colaboradores: {dep.Colaboradores}]");
+            }
+        }
+        
+        static void SemChavePrimaria()
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            var usuariosFuncoes = db.UsuarioFuncoes.Where(p=>p.UsuarioId == Guid.NewGuid()).ToArray();
+        }
+
+        static void SingleOrDefaultVsFirstOrDefault()
+        {
+            using var db = new ApplicationContext();
+
+            Console.WriteLine("SingleOrdefault: ");
+
+            _ = db.Departamentos.SingleOrDefault(p => p.Id > 2);
+
+            Console.WriteLine("FirstOrDefault: ");
+
+            _ = db.Departamentos.FirstOrDefault(p => p.Id > 2);
+        }
+
+        static void ConsultaFiltrada()
+        {
+            using var db = new ApplicationContext();
+
+            var sql = db.Departamentos
+                .Include(p => p.Colaboradores.Where(c => c.Nome.Contains("Teste")))
+                .ToQueryString();
+
+           Console.WriteLine(sql);
+        }
+
+        static void Clear()
+        {
+            using var db = new ApplicationContext();
+
+            db.Departamentos.Add(new Departamento { Descricao = "TESTE DebugView" });
+
+            db.ChangeTracker.Clear();
+        }
+
+        static void DebugView()
+        {
+            using var db = new ApplicationContext();
+
+            db.Departamentos.Add(new Departamento { Descricao = "TESTE DebugView" });
+
+            var query = db.Departamentos.Where(p => p.Id > 2);
+        }
+
+        static void ToQueryString()
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.EnsureCreated();
+
+            var query = db.Departamentos.Where(p => p.Id > 2);
+
+            var sql = query.ToQueryString();
+
+            Console.WriteLine(sql);
+        }
+    }
+
+}
